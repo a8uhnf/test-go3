226c1e5f9f5fa331e98d696a081b9de30d975743
hanif <hanif@appscode.com>
2017-11-23 15:02:07 +0600

    Initial commit

---
+++ hello_world.go
@@ -0,0 +1,13 @@
+package test_go2
+
+import (
+	"fmt"
+)
+
+func HelloWorld()  {
+	fmt.Println("Hello World from test-go2")
+}
+
+func HelloWorldTestDep()  {
+	fmt.Println("Hello World from test-go2. Branch: test-dep")
+}
